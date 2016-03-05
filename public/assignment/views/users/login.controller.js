6c0670578131c8478871651a58fe1420fb2ee059
Manu Saxena <manusaxena@Manus-MacBook-Air.local>
2016-03-05 03:03:13 -0500

    Assignment 2 Client side devlopment

--- public/assignment/views/users/login.controller.js
+++ public/assignment/views/users/login.controller.js
@@ -3,11 +3,10 @@
         .module("FormBuilderApp")
         .controller("LoginController", LoginController);
 
-    function loginController ($scope, UserService, $location, $rootScope) {
+    function LoginController ($scope, UserService, $location, $rootScope) {
         $scope.login = login;
 
         function login (user) {
-
             UserService.findUserByCredentials(user.username,user.password,function(ret_user)
             {
                 if (ret_user) {
