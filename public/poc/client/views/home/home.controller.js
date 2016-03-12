6c3a6c24be15ee0de2a2c9f5a658868bdcbd4fa2
Manu Saxena <manusaxena@Manus-MacBook-Air.local>
2016-03-11 20:02:30 -0500

    added maps to poc

--- public/poc/client/views/home/home.controller.js
+++ public/poc/client/views/home/home.controller.js
@@ -4,6 +4,27 @@
         .controller("HomeController", HomeController);
 
     function HomeController($scope, $location) {
+
         $scope.$location = $location;
+        var lat;
+        var long;
+
+        navigator.geolocation.getCurrentPosition(success);
+
+        function success(position) {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+        }
+
+        function init() {
+            var mapProp = {
+                center: new google.maps.LatLng(lat, long),
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        }
+
+        google.maps.event.addDomListener(window, 'load', init);
     }
 })();
